54a6e2c948a2178ff9c5e02546e4725daf26ea0a
Nicolas Decat <nicolas.decat@gmail.com>
2020-08-07 13:07:06 +0200

    first commit

---
+++ src/main/java/com/nd2k/server_mongo/ServerMongoApplication.java
@@ -0,0 +1,13 @@
+package com.nd2k.server_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nd2k/server_mongo/ServerMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nd2k.server_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
